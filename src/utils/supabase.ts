e7480271096a8102e14e4a95e3b5fae1607df4d2
DestroyerZzz2 <detroyerkgb@gmail.com>
2025-07-13 21:54:56 +0300

    Update supabase.ts

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,19 +2,10 @@ import { createBrowserClient } from '@supabase/ssr'
 import { SUPABASE_CONFIG } from './supabase-config'
 
 // Create a single client instance that will be reused
-const originalClient = createBrowserClient(
+const supabaseClient = createBrowserClient(
   SUPABASE_CONFIG.url,
   SUPABASE_CONFIG.anonKey
 )
-
-// Wrap the auth.getUser method to add debugging
-const originalGetUser = originalClient.auth.getUser.bind(originalClient.auth)
-originalClient.auth.getUser = async () => {
-  console.log('🚨 AUTH.GETUSER() CALLED FROM:', new Error().stack)
-  return originalGetUser()
-}
-
-const supabaseClient = originalClient
 
 export function createClient() {
   return supabaseClient
